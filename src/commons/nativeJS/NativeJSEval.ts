ea793371930cb5d686296994ee818f3144093e84
shenyihong <shen_yi_hong@outlook.com>
2021-12-30 17:25:36 +0800

    sicp integration to nativeJS
    - added way to restrict access to globals via prelude

--- src/commons/nativeJS/NativeJSEval.ts
+++ src/commons/nativeJS/NativeJSEval.ts
@@ -1,17 +1,36 @@
 /* eslint-disable no-eval */
+import 'sicp';
+
 import { ExceptionError } from 'js-slang/dist/errors/errors';
 import { NativeJSEvalResult } from 'src/features/nativeJS/NativeJSTypes';
 
 import { getEvalErrorLocation } from './EvalErrorLocator';
 
+const RESTRICTED_GLOBALS: string[] = ['window'];
+const RESTRICTED_GLOBALS_PRELUDE = `let ${RESTRICTED_GLOBALS.join(',')}`;
+const EVAL_PRELUDE = `${[RESTRICTED_GLOBALS_PRELUDE].join('\n')}\n`;
+
+interface EvalFuncPreludeOffset {
+  evalFunc: (program: string) => any;
+  preludeOffset: number;
+}
+
+const restrictedEval: EvalFuncPreludeOffset = {
+  // modules are in strict mode by default
+  // therefore, `eval()` will be ran in strict mode
+  evalFunc: (program: string) => eval(`${EVAL_PRELUDE}${program}`),
+  preludeOffset: EVAL_PRELUDE.split('\n').length - 1
+};
+
 export function evalNativeJSProgram(program: string): NativeJSEvalResult {
+  const { evalFunc, preludeOffset }: EvalFuncPreludeOffset = restrictedEval;
   try {
-    return { status: 'finished', value: eval(program) };
+    return { status: 'finished', value: evalFunc(program) };
   } catch (error) {
     return {
       status: 'error',
       value: undefined,
-      error: new ExceptionError(error, getEvalErrorLocation(error))
+      error: new ExceptionError(error, getEvalErrorLocation(error, preludeOffset))
     };
   }
 }
